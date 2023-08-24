93d1fc0f1d35734f742b34bd1c8ac16e6fa3f445
sabrinaduque <sabrinanduque@hotmail.com>
2023-08-24 18:27:14 -0300

    add buttonText

---
+++ src/components/ButtonText/styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components"
+
+export const Container = styled.button`
+  color: ${({ theme }) => theme.COLORS.ORANGE};
+  font-size: 16px;
+  background: none;
+  border: none;
+`
--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -3,22 +3,53 @@ import styled from 'styled-components'
 export const Container = styled.div`
   width: 100%;
   height: 100vh;
-
   display: grid;
   grid-template-rows: 105px auto;
   grid-template-areas: 
   "header"
-  "content"
-  ;
+  "content";
+
+  > main {
+    grid-area: content;
+    overflow-y: scroll;
+    padding: 64px 0;
+  }
 `
 export const Links = styled.ul`
   list-style: none;
 
-  >li {
+  > li {
     margin-top: 12px;
   }
 
   a {
     color: ${({ theme }) => theme.COLORS.WHITE};
+  }
+`
+
+export const Content = styled.div`
+  max-width:550px;
+  margin: 0 auto;
+
+  display: flex;
+  flex-direction: column;
+
+  > button:first-child {
+    align-self: end;
+  }
+
+  > h1 {
+    color: ${({ theme }) => theme.COLORS.WHITE};
+    font-size: 36px;
+    font-weight: 500;
+    margin-top: 64px;
+  }
+
+  > p {
+    color: ${({ theme }) => theme.COLORS.WHITE};
+    font-size: 16px;
+    margin-top: 16px;
+    font-weight: 400;
+    text-align: justify;
   }
 `
