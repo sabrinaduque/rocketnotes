0c4520d8bf29334b5056af60f57b29b6a6e9fbf5
sabrinaduque <sabrinanduque@hotmail.com>
2023-08-28 12:11:30 -0300

    add profile page

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { SignUp } from './pages/SignUp/index'
+import { Profile } from './pages/Profile/index'
 import { ThemeProvider } from 'styled-components'
 import theme from './styles/theme'
 import GlobalStyle from './styles/global'
@@ -9,7 +9,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle/>
-      <SignUp/>
+      <Profile/>
     </ThemeProvider>
   </React.StrictMode>,
 )
