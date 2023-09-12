76729406e2499df31508d46ecc48e73b5bed7f4a
sabrinaduque <sabrinanduque@hotmail.com>
2023-09-12 17:06:17 -0300

    implements logout functionality

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,10 @@
 import { RiShutDownLine } from "react-icons/ri";
 import { Container, Profile, Logout } from "./styles";
+import { useAuth } from "../../hooks/auth"
 
 export function Header() {
+  const { signOut } = useAuth()
+
   return (
     <Container>
       <Profile to="/profile">
@@ -16,7 +19,7 @@ export function Header() {
         </div>
       </Profile>
 
-      <Logout>
+      <Logout onClick={signOut}>
         <RiShutDownLine />
       </Logout>
     </Container>
